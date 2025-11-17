a5e10163d0f64e5c3a994f9e14d7fc705e69878f
Samrat Sarotra <ssarotra@gmail.com>
2025-11-18 00:26:41 +0530

    change

--- ShoppingPlatform/Services/SmsGatewayService.cs
+++ ShoppingPlatform/Services/SmsGatewayService.cs
@@ -30,6 +30,8 @@ namespace ShoppingPlatform.Services
 
             // Exact template — **match DLT**: no extra spaces after en-dash
             var template = "Dear Customer,\nYour one-time password (OTP) for login to YOBHA is {0}. Please do not share this OTP with anyone for security reasons.\n–YOBHA";
+            var template2 = $"Dear Customer,\nYour one-time password (OTP) for login to YOBHA is {otp}. Please do not share this OTP with anyone for security reasons.\n–YOBHA";
+
             var message = string.Format(template, otp);
             _logger.LogInformation("DEBUG SMS message template: [{template}]" + template);
 
@@ -53,7 +55,7 @@ namespace ShoppingPlatform.Services
                 $"&senderid={Uri.EscapeDataString(_senderId)}" +
                 $"&channel=2&DCS=0&flashsms=0" +
                 $"&number={Uri.EscapeDataString(normalizedNumber)}" +
-                $"&text={message}" +
+                $"&text={template2}" +
                 $"&route=1" +
                 $"&EntityId={Uri.EscapeDataString(entityId)}" +
                 $"&dlttemplateid={Uri.EscapeDataString(dlttemplateid)}";
