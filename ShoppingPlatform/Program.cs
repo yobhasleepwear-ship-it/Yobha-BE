ef66b4e0688bb566aa73f32989944f3cc9278eac
Samrat Sarotra <samrat.sarotra@trukker.com>
2026-01-17 13:17:57 +0530

    s

--- ShoppingPlatform/Program.cs
+++ ShoppingPlatform/Program.cs
@@ -44,11 +44,11 @@ builder.Services.AddCors(options =>
 
                 // Known test hosts
                 if (uri.Host.Equals("yobha-test-env.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
-                if (uri.Host.Equals("https://yobha.world", StringComparison.OrdinalIgnoreCase)) return true;
                 if (uri.Host.Equals("yobha.world", StringComparison.OrdinalIgnoreCase)) return true;
                 if (uri.Host.Equals("yobha-test-env-aef5.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
                 if (uri.Host.Equals("yobha-frontend-user-test.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
                 if (uri.Host.Equals("yobha-frontend-admin-test.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
+                if (uri.Host.Equals("backend.yobha.world", StringComparison.OrdinalIgnoreCase)) return true;
 
                 // Allow Vercel preview branches for this project only, e.g., yobha-test-env-git-<branch>-<hash>.vercel.app
                 if (uri.Host.EndsWith(".vercel.app", StringComparison.OrdinalIgnoreCase) &&
