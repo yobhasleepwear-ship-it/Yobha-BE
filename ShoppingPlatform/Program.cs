444c080c1bad0476be036a861912061c34bcd7b8
Samrat Sarotra <ssarotra@gmail.com>
2026-01-19 10:09:17 +0530

    s

--- ShoppingPlatform/Program.cs
+++ ShoppingPlatform/Program.cs
@@ -44,7 +44,9 @@ builder.Services.AddCors(options =>
 
                 // Known test hosts
                 if (uri.Host.Equals("yobha-test-env.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
-                if (uri.Host.Equals("yobha.world", StringComparison.OrdinalIgnoreCase)) return true;
+                if (uri.Host.Contains("yobha.world", StringComparison.OrdinalIgnoreCase)) return true;
+                if (uri.Host.Equals("www.yobha.world", StringComparison.OrdinalIgnoreCase)) return true;
+
                 if (uri.Host.Equals("yobha-test-env-aef5.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
                 if (uri.Host.Equals("yobha-frontend-user-test.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
                 if (uri.Host.Equals("yobha-frontend-admin-test.vercel.app", StringComparison.OrdinalIgnoreCase)) return true;
